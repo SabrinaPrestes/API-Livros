4e168d9b3b5008b850054780af29a9ce81b9acea
SabrinaPrestes <sabrinaprestes25@gmail.com>
2025-05-22 10:23:56 -0300

    Entidades JPA

--- src/main/java/gerenciamento/livros_api/domain/Book.java
+++ src/main/java/gerenciamento/livros_api/domain/Book.java
@@ -1,8 +1,22 @@
 package gerenciamento.livros_api.domain;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "books",
+        uniqueConstraints = @UniqueConstraint(name = "UK_BOOK_ISBN", columnNames = "isbn"))
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Book {
 
-    public static void main(String[] args) {
-
-    }
+    @Id @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String author;
+    private Integer publicationYear;
+    private String isbn;
 }
--- src/main/java/gerenciamento/livros_api/dto/CreateBookRequest.java
+++ src/main/java/gerenciamento/livros_api/dto/CreateBookRequest.java
@@ -1,6 +1,7 @@
 package gerenciamento.livros_api.dto;
 
 public record CreateBookRequest(
+        Long id,
         String title,
         String author,
         Integer publicationYear,
